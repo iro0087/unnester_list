93b5110f578d7d345d571838f7064a0d3d143560
Larget-Piet Julien <114911243+iro0087@users.noreply.github.com>
2024-01-14 22:41:49 +0100

    Add files via upload

--- unnester.py
+++ unnester.py
@@ -12,39 +12,25 @@ class unnester():
 
     def end_(input_l, rtn_l, flag_l, dim_end):
 
-        print("end_", rtn_l)
-
-        print(flag_l)
-
         idx = -1
 
         adjust = 1
 
         for el in range(len(flag_l)):
 
-            print(flag_l[el])
-
             if el == 0:
-
-                print("zero", unnester.nestfind(input_l, flag_l[el]))
 
                 idx += len(unnester.nestfind(input_l, flag_l[el])) + flag_l[el][-1]
 
             elif len(flag_l[el]) == len(flag_l[el - 1]):
 
-                print("one", unnester.nestfind(input_l, flag_l[el]))
-
                 idx += len(unnester.nestfind(input_l, flag_l[el])) + (flag_l[el][-1] - flag_l[el - 1][-1]) - 1
 
             elif len(flag_l[el]) > len(flag_l[el - 1]): 
 
-                print("two")
-
                 idx += len(unnester.nestfind(input_l, flag_l[el])) - (len(unnester.nestfind(input_l, flag_l[el - 1])) - flag_l[el][-1])
 
             else:
-
-                print("three")
 
                 len_flag_l_curr = len(flag_l[el])
 
@@ -54,17 +40,11 @@ class unnester():
 
                     remain_idx += len(unnester.nestfind(input_l, flag_l[el - 1][0:len(flag_l[el - 1]) - i])) - flag_l[el - 1][-i] - 1
 
-                print("remain_idx: ", remain_idx)
-
                 idx += len(unnester.nestfind(input_l, flag_l[el])) + (flag_l[el][-1] - flag_l[el - 1][len_flag_l_curr - 1]) - 1 + remain_idx 
-
-            print("idx: ", idx, el)
 
             if len(flag_l[el]) == dim_end:
 
                 cur = unnester.nestfind(input_l, flag_l[el])
-
-                print("cur: ", cur, rtn_l[idx - len(cur):idx], idx)
 
                 rtn_l[idx - len(cur) + adjust:idx + adjust] = [cur]
 
@@ -78,29 +58,17 @@ class unnester():
 
         wrk_l_pre = []
 
-        print("strt_l1: ", strt_l)
-
-        print("id_rec_main depart: ", id_rec_main)
-
         for i in range(len(strt_l)):
-
-            print("ok", strt_l[i])
 
             wrk_l_pre.append(wrk_l)
 
             if type(wrk_l[strt_l[i]]) != list:
-
-                print(wrk_l[strt_l[i]])
 
                 wrk_l = [wrk_l[strt_l[i]]]
 
             else:
 
                 wrk_l = wrk_l[strt_l[i]]
-
-        print("wrk_l1: ", wrk_l)
-
-        print("wrk_l_pre: ", wrk_l_pre)
 
         list_status = False
 
@@ -112,13 +80,9 @@ class unnester():
 
             cnt = 0
 
-        print("cnt: ", cnt)
-
         while list_status == False and cnt < len(wrk_l):
 
             if type(wrk_l[cnt]) == list:
-
-                print("detected", dim_end)
 
                 strt_l.append(cnt)
 
@@ -134,19 +98,11 @@ class unnester():
 
             cnt += 1
 
-        print("id_rec_main: ", id_rec_main)
-
-        print("rtn_l: ", rtn_l)
-
         if list_status == False:
 
             if len(strt_l) > 0:
 
-                print("ici", len(wrk_l_pre[-1]))
-
                 if strt_l[-1] + 1 == len(wrk_l_pre[-1]):
-
-                    print("sup")
 
                     if len(strt_l) == 1:
 
@@ -164,19 +120,9 @@ class unnester():
 
                     next_ = False
 
-                    print("oup")
-
-                    print(strt_l)
-
-                    print(wrk_l_pre[-1])
-
                     while next_ == False and len(strt_l) > 0:
 
-                        print("len: ", len(wrk_l_pre[-1]), "strt_l: ", strt_l[-1], strt_l)
-
                         if strt_l[-1] + 1 < len(wrk_l_pre[-1]):
-
-                            print("nextt", strt_l[-1], wrk_l_pre[-1])
 
                             if strt_l[-1] < len(wrk_l_pre[-1]) - 1:
 
@@ -188,15 +134,11 @@ class unnester():
 
                                     if strt_l[-1] == len(wrk_l_pre[-1]) - 1:
 
-                                        print("next2b")
-
                                         if len(strt_l) == 1:
 
                                             id_rec_main += 1
 
                                             strt_l.pop()
-
-                                            print("id_rec_main: ", id_rec_main)
 
                                             if id_rec_main == len(input_l):
 
@@ -205,8 +147,6 @@ class unnester():
                                         stop = 1
 
                                     else:
-
-                                        print("next2", dim_end)
 
                                         if  type(wrk_l_pre[-1][strt_l[-1]]) == list:
 
@@ -227,8 +167,6 @@ class unnester():
 
                                 strt_l[-1] += 1
 
-                                print("ff")
-
                                 if len(strt_l) == 1:
 
                                     id_rec_main += 1
@@ -241,15 +179,11 @@ class unnester():
 
                         else:
 
-                            print("oco")
-
                             strt_l.pop()
 
                             wrk_l_pre.pop() 
 
                             if len(strt_l) == 0:
-
-                                print("ici2")
 
                                 id_rec_main += 1
 
@@ -261,15 +195,11 @@ class unnester():
 
                     id_rec_main += 1
 
-                    print("ici id_rec_main: ", id_rec_main, rtn_l)
-
                     if id_rec_main == len(input_l):
 
                         return unnester.end_(input_l, rtn_l, flag_l, dim_end)
 
             elif id_rec_main == len(input_l):
-
-                print("quoi", rtn_l)
 
                 return unnester.end_(input_l, rtn_l, flag_l, dim_end) 
 
@@ -279,33 +209,13 @@ class unnester():
 
                 if strt_l != flag_l[-1]:
 
-                    print("append: ", strt_l)
-
                     flag_l.append([ el for el in strt_l ])
 
             else:
 
-                print("append: ", strt_l)
-
                 flag_l.append([ el for el in strt_l ])
-
-        print("strt_lf: ", strt_l)
-
-        print("rtn_lf: ", rtn_l)
-
-        print("id_rec_mainf: ", id_rec_main)
-
-        print("")
-
-        print("#######################")
-
-        print("")
 
         return unnester.ns(input_l, dim_end, strt_l, rtn_l, id_rec_main, wrk_l, flag_l)
 
-x = unnester.ns([1, [5, [[2], 4, [23, 3, 3]]], 2, 3334, [4, [55, 56], 7, [77, [66, 67], 78], 2, [33, 5]], 3, [5, 6], 4], 3)
 
-print("")
-
-print("return: ", x)
 
